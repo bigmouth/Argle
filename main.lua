b9c19cbca39e73381aeb1d120df00c1d6fa20756
Gabriele Boccone <gabriele.boccone@ncr.com>
2018-03-26 09:25:38 +0200

    Now we have walls!

--- main.lua
+++ main.lua
@@ -10,26 +10,40 @@ HEIGHT = 6
 TILE_SIZE = 64
 PADDING = 2
 
+TILE_NONE = 0
+TILE_BALL = 1
+TILE_WALL = 2
+
+tiles = {}
+grid = { balls = {{3, 2}, {3, 4}, {5, 4}}, walls = {{2, 2}, {2, 5}, {7, 2}, {7, 5}} }
+
+function makeTile(x, y, type)
+  tiles[y][x] = { type = type, x = x, y = y, tx = x, ty = y }
+end
+
 function love.load()
   Object = require "classic"
-  
-  tiles = {}
-  
+
   for i = 1, HEIGHT do
     tiles[i] = {}
     for j = 1, WIDTH do
-      tiles[i][j] = { type = 0, x = j, y = i, tx = j, ty = i }
+      makeTile(j, i, TILE_NONE)
     end
   end
-  
-  tiles[2][3] = {type = 1, x = 3, y = 2, tx = 3, ty = 2}
-  tiles[4][3] = {type = 1, x = 3, y = 4, tx = 3, ty = 4}
-  tiles[4][5] = {type = 1, x = 5, y = 4, tx = 5, ty = 4}
-  
+
+  for _, i in ipairs(grid.balls) do
+    print(i[0])
+    makeTile(i[1], i[2], TILE_BALL)
+  end
+
+  for _, i in ipairs(grid.walls) do
+    makeTile(i[1], i[2], TILE_WALL)
+  end
+
   moving = MOVE_NONE
-  
+
   love.window.setMode(
-    TILE_SIZE * WIDTH + 2 * PADDING, 
+    TILE_SIZE * WIDTH + 2 * PADDING,
     TILE_SIZE * HEIGHT+ 2 * PADDING,
     {})
 end
@@ -40,30 +54,30 @@ function love.update(dt)
     for i = 1, HEIGHT do
       for j = 1, WIDTH do
         t = tiles [i][j]
-        if t.type == 1 then
+        if t.type == TILE_BALL then
           if moving == MOVE_SOUTH then
-            t.y = t.y + dt * 5
+            t.y = t.y + dt * 9
             if t.y > t.ty then
               t.y = t.ty
             else
               next = MOVE_SOUTH
             end
           elseif moving == MOVE_NORTH then
-            t.y = t.y - dt * 5
+            t.y = t.y - dt * 9
             if t.y < t.ty then
               t.y = t.ty
             else
               next = MOVE_NORTH
             end
           elseif moving == MOVE_WEST then
-            t.x = t.x - dt * 5
+            t.x = t.x - dt * 9
             if t.x < t.tx then
               t.x = t.tx
             else
               next = MOVE_WEST
             end
           elseif moving == MOVE_EAST then
-            t.x = t.x + dt * 5
+            t.x = t.x + dt * 9
             if t.x > t.tx then
               t.x = t.tx
             else
@@ -76,42 +90,44 @@ function love.update(dt)
     moving = next
     if next == MOVE_NONE then
       ntiles = {}
-      
+
       for i = 1, HEIGHT do
         ntiles[i] = {}
         for j = 1, WIDTH do
-          ntiles[i][j] = { type = 0, x = j, y = i, tx = j, ty = i }
+          ntiles[i][j] = { type = TILE_NONE, x = j, y = i, tx = j, ty = i }
         end
       end
-      
+
       for i = 1, HEIGHT do
         for j = 1, WIDTH do
           t = tiles[i][j]
-          if t.type > 0 then
+          if t.type ~= TILE_NONE then
             ntiles[t.y][t.x] = t
             print ("in: " ..t.x .. ":" .. t.y .. " -> " .. t.x .. ":" .. t.y)
           end
         end
       end
-      
+
       tiles = ntiles
     end
   end
 end
 
 function love.keypressed(key, scancode, isrepeat)
-  if moving == MOVE_NONE then    
+  if moving == MOVE_NONE then
     if key == 's' then
       moving = MOVE_SOUTH
       for j = 1, WIDTH do
         bottom = 0
         for i = HEIGHT, 1, -1 do
           t = tiles[i][j]
-          if bottom == 0 and t.type == 0 then
+          if bottom == 0 and t.type == TILE_NONE then
             bottom = i
-          elseif bottom > 0 and t.type == 1 then
+          elseif bottom > 0 and t.type == TILE_BALL then
             t.ty = bottom
             bottom = bottom - 1
+          elseif t.type == TILE_WALL then
+            bottom = 0
           end
         end
       end
@@ -121,12 +137,13 @@ function love.keypressed(key, scancode, isrepeat)
         top = 0
         for i = 1, HEIGHT do
           t = tiles[i][j]
-          if top == 0 and t.type == 0 then
+          if top == 0 and t.type == TILE_NONE then
             top = i
-          elseif top > 0 and t.type == 1 then
+          elseif top > 0 and t.type == TILE_BALL then
             t.ty = top
             top = top + 1
-            print("now: " .. t.x .. ":" .. t.y .. " to " .. t.tx .. ":" .. t.ty)
+          elseif t.type == TILE_WALL then
+            top = 0
           end
         end
       end
@@ -136,28 +153,32 @@ function love.keypressed(key, scancode, isrepeat)
         left = 0
         for j = 1, WIDTH do
           t = tiles[i][j]
-          if left == 0 and t.type == 0 then
+          if left == 0 and t.type == TILE_NONE then
             left = j
-          elseif left > 0 and t.type == 1 then
+          elseif left > 0 and t.type == TILE_BALL then
             t.tx = left
             left = left + 1
+          elseif t.type == TILE_WALL then
+            left = 0
           end
         end
-      end      
+      end
     elseif key == 'd' then
       moving = MOVE_EAST
       for i = 1, HEIGHT do
         right = 0
         for j = WIDTH, 1, -1 do
           t = tiles[i][j]
-          if right == 0 and t.type == 0 then
+          if right == 0 and t.type == TILE_NONE then
             right = j
-          elseif right > 0 and t.type == 1 then
+          elseif right > 0 and t.type == TILE_BALL then
             t.tx = right
             right = right - 1
+          elseif t.type == TILE_WALL then
+            right = 0
           end
         end
-      end      
+      end
     end
   end
 end
@@ -167,9 +188,11 @@ function love.draw()
   for i = 1, HEIGHT do
     for j = 1, WIDTH do
       t = tiles[i][j]
-      if t.type == 1 then
-        love.graphics.circle("fill", t.x * TILE_SIZE - TILE_SIZE / 2 + PADDING, t.y * TILE_SIZE - TILE_SIZE / 2 + PADDING, 32)
-      end      
+      if t.type == TILE_BALL then
+        love.graphics.circle("fill", t.x * TILE_SIZE - TILE_SIZE / 2 + PADDING, t.y * TILE_SIZE - TILE_SIZE / 2 + PADDING, TILE_SIZE / 2)
+      elseif t.type == TILE_WALL then
+        love.graphics.rectangle("fill", t.x * TILE_SIZE - TILE_SIZE + PADDING, t.y * TILE_SIZE - TILE_SIZE + PADDING, TILE_SIZE, TILE_SIZE)
+      end
     end
   end
 end
